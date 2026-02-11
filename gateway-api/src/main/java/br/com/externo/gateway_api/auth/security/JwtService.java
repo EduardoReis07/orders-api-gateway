1db7d5e6785bd231fa63b5d8f19450ba4269f1be
EduardoReis07 <eduardogreis21@outlook.com>
2026-02-11 10:19:49 -0300

    feat(gateway): secret-key transferida para o application.properties

--- gateway-api/src/main/java/br/com/externo/gateway_api/auth/security/JwtService.java
+++ gateway-api/src/main/java/br/com/externo/gateway_api/auth/security/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,8 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "b157db3c62d18254d200ed4bae6aada6c22cf7ff098ff50e57889f12dd53b99d";
+    @Value("${jwt.secret-key}")
+    private String secretKey;
 
     public String extractUsername(String token) {
         return extractAllClaims(token).getSubject();
@@ -66,7 +68,7 @@ public class JwtService {
     }
 
     private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
